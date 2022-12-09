bd23d57b9254bef98de58904a4da8a0ed91736b0
Saraengdahlnilsson <saraengdahl@live.se>
2022-12-09 08:01:39 +0100

    bok id

--- script.js
+++ script.js
@@ -19,11 +19,65 @@ searchField.addEventListener('keyup', (e) =>
   )
 );
 
+document.getElementById('root').addEventListener('mouseover', (e) => {
+  if (e.target.className.indexOf('book-details') >= 0) {
+
+    return;
+  }
+  if (!bookId) {
+  const bookId = e.target.dataset?.id;
+
+  toggleModal(false);
+  return;
+}
+
+if (!bookDetailsModal) {
+  renderDetailsModal(e);
+}
+
+
+toggleModal(true, e);
+
+getById(bookId).then(apiBook => {
+  bookDetailsModal.innerText = '';
+  bookDetailsModal.insertAdjacentHTML('beforeend', BookDetails(apiBook));
+});
+});
+
 function renderBookList(bookList) {
   const existingElement = document.querySelector('.book-list');
-
   const root = document.getElementById('root');
-
   existingElement && root.removeChild(existingElement);
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+}
+const root = document.getElementById('root');
+const modal = document.createElement('div');
+modal.id = 'bookDetails';
+modal.className = 'book-details absolute p-3 rounded-md border-2 border-blue-400 bg-white';
+modal.style.display = 'none';
+function renderDetailsModal(e) {
+ 
+  root.appendChild(modal);
+
+  bookDetailsModal = modal;
+}
+
+function toggleModal(show, e) {
+  if (!bookDetailsModal) {
+    return;
+  }
+  bookDetailsModal.innerText = '';
+
+  if (show === true) {
+    bookDetailsModal.style.display = 'block';
+    bookDetailsModal.style.left = e.clientX + 'px';
+    bookDetailsModal.style.top = e.clientY + 'px';
+  } else {
+
+    bookDetailsModal.style.display = 'none';
+  }
+}
+
+if (!bookDetailsModal) {
+  renderDetailsModal(e);
 }
